5c5835d9396ea1ab767922a6cb7a31e3af1d6030
Juan Jose Aristizabal <juanesaristisabal@gmail.com>
2021-05-20 17:14:44 -0500

    interrupt/keyboard

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,18 @@
 #include <kernel/vga.h>
+#include <kernel/cpu.h>
+#include <kernel/keyboard.h>
 
 void main() {
 	set_color(BROWN, YELLOW);
 	clean_screen();
-	print_string("Hello from kernel\n");
+	
+	print_string("Starting OS kernel\n");
+
+	isr_install();
+	
+	print_string("Enabling external interrupts.\n");
+	asm volatile("sti");
+
+	print_string("Initializing keyboard (IRQ 1).\n");
+	init_keyboard();
 }
